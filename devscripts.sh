9699b052ba0fa2270502c590244e5fa954d8f12e
sutt <wsutton17@gmail.com>
2025-06-15 10:28:09 -0400

    chore: create devscripts.sh

--- devscripts.sh
+++ devscripts.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+# initial setup
 set -e
 set -a
 
+# default args
 API_IMAGE_NAME="type-friend:latest"
 API_CONTAINER_NAME="type-friend-container"
 API_MAPPED_PORT=8000
 
+# override default args
 if [ -f .env ]; then
   . ./.env
 else
     echo "WARNING: could not find .env file. Running with all default args"
 fi
 
+# function: deploy / re-deploy api container
 docker build -t "$API_IMAGE_NAME" .
 docker stop "$API_CONTAINER_NAME" && docker rm "$API_CONTAINER_NAME" || true
 docker run -d --name "$API_CONTAINER_NAME" -p "$API_MAPPED_PORT":8000 "$API_IMAGE_NAME"
